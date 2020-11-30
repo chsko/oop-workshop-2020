adaf0b36ba910130251c626df0c43cab07738889
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2020-11-30 21:28:37 +0100

    Weighted vertices in graph

--- src/main/kotlin/graph/Node.kt
+++ src/main/kotlin/graph/Node.kt
@@ -1,16 +1,42 @@
 package graph
 
 class Node {
-    private val neighbors = mutableListOf<Node>()
+    private val vertices = mutableListOf<Vertex>()
 
-    private fun canReach(destination: Node, visited: MutableList<Node>): Boolean =
-        when {
-            destination in (neighbors + this) -> true
-            this in visited -> false
-            else -> visited.add(this) && neighbors.any { it.canReach(destination, visited) }
+    companion object {
+        private val UNREACHABLE = Double.POSITIVE_INFINITY
+    }
+
+    infix fun canReach(destination: Node) = this.minCost(destination, emptyList()) != UNREACHABLE
+
+    private fun minCost(destination: Node, visited: List<Node>): Double {
+        return when {
+            destination == this -> 0.0
+            this in visited -> UNREACHABLE
+            else -> vertices.minOfOrNull { vertex -> vertex.minCost(destination, visited + this) } ?: UNREACHABLE
         }
+    }
 
-    infix fun canReach(destination: Node) = this.canReach(destination, mutableListOf())
+    infix fun minCost(destination: Node) = minCost(destination, listOf()).also {
+        require(it != UNREACHABLE) { "There is no edge to this destination" }
+    }.toInt()
 
-    infix fun to(destination: Node) = neighbors.add(destination).let { destination }
+    infix fun costs(amount: Number): VertexBuilder {
+        return VertexBuilder(this, amount.toDouble())
+    }
+
+    infix fun connectsTo(target: Node) = this.costs(1.0).connectsTo(target)
+
+    private fun addVertex(target: Vertex) {
+        vertices.add(target)
+    }
+
+    // Understands how to connect to a Node
+    private class Vertex(private val target: Node, private val cost: Double = 1.0) {
+        fun minCost(destination: Node, visited: List<Node>) = target.minCost(destination, visited) + cost
+    }
+
+    class VertexBuilder(private val node: Node, private val amount: Double) {
+        infix fun connectsTo(target: Node) = target.also { node.addVertex(Vertex(target, amount)) }
+    }
 }
--- src/test/kotlin/graph/NodeTest.kt
+++ src/test/kotlin/graph/NodeTest.kt
@@ -2,6 +2,8 @@ package graph
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 internal class NodeTest {
     companion object {
@@ -16,11 +18,11 @@ internal class NodeTest {
     }
 
     init {
-        nodeB to nodeC to nodeD to nodeE to nodeB to nodeF
-        nodeB to nodeA
-        nodeC to nodeD
-        nodeC to nodeE
-        nodeH to nodeB
+        nodeB connectsTo nodeC connectsTo nodeD connectsTo nodeE connectsTo nodeB connectsTo nodeF
+        nodeB connectsTo nodeA
+        nodeC connectsTo nodeD
+        nodeC connectsTo nodeE
+        nodeH connectsTo nodeB
     }
 
     @Test
@@ -43,5 +45,29 @@ internal class NodeTest {
         assertFalse(nodeB canReach nodeH)
         assertFalse(nodeC canReach nodeH)
         assertFalse(nodeD canReach nodeG)
+    }
+
+    @Test
+    fun minCost() {
+        assertEquals(3, nodeC minCost nodeF)
+        assertEquals(1, nodeB minCost nodeF)
+        assertEquals(2, nodeD minCost nodeB)
+        assertEquals(0, nodeB minCost nodeB)
+    }
+
+    @Test
+    fun `weighted vertices`() {
+        val nodeA = Node()
+        val nodeB = Node()
+        val nodeC = Node()
+        val nodeD = Node()
+
+        nodeA costs 2 connectsTo nodeB costs 3 connectsTo nodeC connectsTo nodeA
+
+        assertEquals(5, nodeA minCost nodeC)
+        assertEquals(1, nodeC minCost nodeA)
+        assertEquals(4, nodeB minCost nodeA)
+        assertEquals(0, nodeA minCost nodeA)
+        assertThrows<IllegalArgumentException> { nodeD minCost nodeA }
     }
 }
